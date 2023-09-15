2fc0ecd9f584b1945df9c347a29eaf234288ac07
SebaRenner <sebastian.renner@gmx.ch>
2023-09-15 17:18:20 +0200

    Added components and service

--- CrossComponentCommunicationExamples/projects/services/src/app/app.module.ts
+++ CrossComponentCommunicationExamples/projects/services/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SelectionComponent } from './components/selection/selection.component';
+import { DisplayComponent } from './components/display/display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectionComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule

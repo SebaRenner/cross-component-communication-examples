dce97feaaaf87bc361e2880ed19a1de1d41622de
SebaRenner <sebastian.renner@gmx.ch>
2024-06-14 22:22:11 +0200

    removed unused modules from services example

--- projects/services/src/app/app.module.ts
+++ projects/services/src/app/app.module.ts
@@ -4,8 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SelectionComponent } from './components/selection/selection.component';
 import { DisplayComponent } from './components/display/display.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
@@ -14,9 +12,7 @@ import { StoreModule } from '@ngrx/store';
     DisplayComponent
   ],
   imports: [
-    BrowserModule,
-    NgbModule,
-    StoreModule.forRoot({}, {})
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
